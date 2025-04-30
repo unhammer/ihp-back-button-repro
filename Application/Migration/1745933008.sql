f95cf63d7dba524796adc89750ef79eb355237e3
Kevin Brubeck Unhammer <kevin@trigram.no>
2025-04-30 13:17:05 +0200

    include User

---
+++ Application/Migration/1745933008.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users (
+    id UUID DEFAULT uuid_generate_v4() PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL
+);
---
+++ Web/View/User/Edit.hs
@@ -0,0 +1,23 @@
+module Web.View.User.Edit where
+import Web.View.Prelude
+
+data EditView = EditView { user :: User }
+
+instance View EditView where
+    html EditView { .. } = [hsx|
+        {breadcrumb}
+        <h1>Edit User</h1>
+        {renderForm user}
+    |]
+        where
+            breadcrumb = renderBreadcrumb
+                [ breadcrumbLink "Users" UsersAction
+                , breadcrumbText "Edit User"
+                ]
+
+renderForm :: User -> Html
+renderForm user = formFor user [hsx|
+    {(textField #name)}
+    {submitButton}
+
+|]
